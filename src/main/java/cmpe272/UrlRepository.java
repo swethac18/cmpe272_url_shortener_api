a17cf5f31f7a4d6f2450d1ca01c0514ad52d0995
Vijay Samuel <vija@ebay.com>
2017-11-30 13:10:59 -0800

    Adding support for user auth

--- src/main/java/cmpe272/UrlRepository.java
+++ src/main/java/cmpe272/UrlRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface UrlRepository extends MongoRepository<UrlMap, String> {
 	public List<UrlMap> findBysURL(String sURL);
 	public List<UrlMap> findByoURL(String oURL);
+	public List<UrlMap> findByuserName(String userName);
 	public List<UrlMap> findAll();
 }
